13c497d1b503c84e5054475ee144982633471b43
Oleh <potiehin.o@gmail.com>
2023-12-18 23:12:20 +0200

    created middleware and aut controller

--- api/index.js
+++ api/index.js
@@ -1,19 +1,28 @@
 import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
-import userRouter from "./routes/user.route.js"
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 mongoose
   .connect(process.env.MDB_URL)
   .then(console.log("Connected to Mongo DB"))
-  .catch(err => {
-    console.error(err)
+  .catch((err) => {
+    console.error(err);
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on port ${process.env.PORT}`)
 );
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({ success: false, statusCode, message });
+});
