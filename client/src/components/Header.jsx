7d5ae5421ffeefeeec93c99d701b0d756fbe05c7
Oleh <potiehin.o@gmail.com>
2023-12-23 21:00:29 +0200

    quick fix

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,8 +10,8 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Люкс</span>
-            <span className="text-slate-700">Авто</span>
+            <span className="text-slate-500">Auto</span>
+            <span className="text-slate-700">Lux</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
